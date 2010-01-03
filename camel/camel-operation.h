fc802004fb5972207ae59f267b26da1d17e0b965
Matthew Barnes <mbarnes@redhat.com>
2010-01-03 13:00:15 -0600

    Deprecate some CamelOperation functions.
    camel_operation_cancel_block()
    camel_operation_cancel_unblock()
    camel_operation_progress_count()

--- camel/camel-operation.h
+++ camel/camel-operation.h
@@ -53,8 +53,6 @@ CamelOperation *camel_operation_register(CamelOperation *cc);
 void camel_operation_unregister (CamelOperation *cc);
 
 /* called internally by camel, for the current thread */
-void camel_operation_cancel_block(CamelOperation *cc);
-void camel_operation_cancel_unblock(CamelOperation *cc);
 gint camel_operation_cancel_check(CamelOperation *cc);
 gint camel_operation_cancel_fd(CamelOperation *cc);
 #ifdef HAVE_NSS
@@ -66,8 +64,13 @@ CamelOperation *camel_operation_registered(void);
 void camel_operation_start(CamelOperation *cc, const gchar *what, ...);
 void camel_operation_start_transient(CamelOperation *cc, const gchar *what, ...);
 void camel_operation_progress(CamelOperation *cc, gint pc);
-void camel_operation_progress_count(CamelOperation *cc, gint sofar);
 void camel_operation_end(CamelOperation *cc);
+
+#ifndef CAMEL_DISABLE_DEPRECATED
+void camel_operation_cancel_block(CamelOperation *cc);
+void camel_operation_cancel_unblock(CamelOperation *cc);
+void camel_operation_progress_count(CamelOperation *cc, gint sofar);
+#endif /* CAMEL_DISABLE_DEPRECATED */
 
 G_END_DECLS
 
